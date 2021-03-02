3b5aa64acbbb5d23ad2202b250a948f8e3380d71
yango3 <o3.yang@hotmail.com>
2021-03-02 17:08:49 +0800

    To use fisco 2.7.1 SDK

--- src/main/java/com/ruff/hello/Fizz.java
+++ src/main/java/com/ruff/hello/Fizz.java
@@ -1,7 +1,12 @@
 package com.ruff.hello;
 
+
 public class Fizz {
+
+    private static String  configFile = "config.toml";
+
     public static void main(String[] args) {
+
         for (int i = 0; i < 5; i++) {
             System.out.println(convert(i));
         }
