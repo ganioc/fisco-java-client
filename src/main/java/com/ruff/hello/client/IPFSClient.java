b4334a752a055e2cbc69d5b19d3912d7cefe7454
yango3 <o3.yang@hotmail.com>
2021-03-17 17:52:00 +0800

    Add ipfs http push , get interface

--- src/main/java/com/ruff/hello/client/IPFSClient.java
+++ src/main/java/com/ruff/hello/client/IPFSClient.java
@@ -1,29 +1,61 @@
 package com.ruff.hello.client;
 
 import io.ipfs.api.IPFS;
+import io.ipfs.api.MerkleNode;
+import io.ipfs.api.NamedStreamable;
 import io.ipfs.multihash.Multihash;
 import com.ruff.hello.client.Utils.ErrCode;
+
+import java.io.File;
 
 public class IPFSClient {
 
     private String url;
     private IPFS ipfs;
 
-    public IPFSClient(String ip, String port){
-        this.ipfs = new IPFS("/ip4/" + ip + "/tcp/"+ port);
+    public IPFSClient(String ip, String port) {
+        this.ipfs = new IPFS("/ip4/" + ip + "/tcp/" + port);
     }
 
-    public byte[] getByHash(String hash){
+    public byte[] getByHash(String hash) {
         Multihash filePointer = Multihash.fromBase58(hash);
-        try{
+        try {
             byte[] fileContents = this.ipfs.cat(filePointer);
             return fileContents;
-        }catch (Exception e) {
-            byte [] out = {};
+        } catch (Exception e) {
+            byte[] out = {};
             return out;
         }
     }
+    private String push(NamedStreamable.FileWrapper file){
+        try {
+            MerkleNode addResult = ipfs.add(file).get(0);
+            Multihash pointer = addResult.hash;
 
+            return pointer.toString();
+        } catch (Exception e) {
+            return "";
+        }
+    }
+    private String push(NamedStreamable.ByteArrayWrapper file){
+        try {
+            MerkleNode addResult = ipfs.add(file).get(0);
+            Multihash pointer = addResult.hash;
 
+            return pointer.toString();
+        } catch (Exception e) {
+            return "";
+        }
+    }
+
+    public String pushFile(String filename) {
+        NamedStreamable.FileWrapper file = new NamedStreamable.FileWrapper(new File(filename));
+        return this.push(file);
+    }
+    // Push already exist file will get the same HASH code
+    public String pushFile(String filename, byte [] bytes) {
+        NamedStreamable.ByteArrayWrapper file = new NamedStreamable.ByteArrayWrapper(filename, bytes);
+        return this.push(file);
+    }
 
 }
--- src/test/java/com/ruff/hello/client/IPFSClientTest.java
+++ src/test/java/com/ruff/hello/client/IPFSClientTest.java
@@ -1,5 +1,7 @@
 package com.ruff.hello.client;
 
+import io.ipfs.api.MerkleNode;
+import io.ipfs.api.NamedStreamable;
 import io.ipfs.multihash.Multihash;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
@@ -11,13 +13,13 @@ import java.nio.charset.StandardCharsets;
 public class IPFSClientTest {
 
     @Test
-    public void Ipfs(){
-        System.out.println("Test IPFS network:");
+    public void get(){
+        System.out.println("Test IPFS network get :");
 
         IPFSClient client = new IPFSClient("192.168.0.112", "5001");
 
         try{
-            byte[] fileContents = client.getByHash("QmXksBoSt2LagL4UkedPrEL7hx89YsoNwLXhyXejsMSGWf");
+            byte[] fileContents = client.getByHash("QmUrZDGDV6gj9G34wt8Mcz5Z4Yx2jZhUJ4aVkcYsgGCkbD");
             String str = new String(fileContents, StandardCharsets.UTF_8);
             System.out.printf("content:\n");
             System.out.println(str);
@@ -30,6 +32,21 @@ public class IPFSClientTest {
     }
     @Test
     public  void pushFile(){
+        System.out.println("Test IPFS network push :");
 
+        IPFSClient client = new IPFSClient("192.168.0.112", "5001");
+        //IPFS ipfs = new IPFS("/ip4/192.168.0.112/tcp/5001");
+
+//        NamedStreamable.ByteArrayWrapper file = new NamedStreamable.ByteArrayWrapper("hello.txt", "G'day world! IPFS rocks! yang jun.".getBytes());
+
+        try{
+            // List<MerkleNode> lst = ipfs.add(file);
+            String hash = client.pushFile("a.txt","Hello world, everybody!".getBytes(StandardCharsets.UTF_8));
+            System.out.println("fb:" + hash);
+
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+        Assertions.assertEquals(1,1);
     }
 }
