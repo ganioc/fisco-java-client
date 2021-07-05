9c6f891dd221de7e4ba1bc17f5a8d0377885b0cf
yango3 <o3.yang@hotmail.com>
2021-07-05 15:11:37 +0800

    udpate PPayTest

--- src/test/java/com/ruff/hello/client/PPayTest.java
+++ src/test/java/com/ruff/hello/client/PPayTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Assertions;
 public class PPayTest {
 
     static String privateKey = "cdc7cc95755f19aa8168e2b0c3dd89d556be87b60608835549c0aee38d156640";
-    static String contractAddress = "0x3d215cd9553b033b35af84dd38951cc8562908c5";
+    static String contractAddress = "0xe78436719865cc9b22b9ffd03da68b7e07c16864";// ""0x3d215cd9553b033b35af84dd38951cc8562908c5";
 
     @Test
     public void query(){
@@ -20,11 +20,18 @@ public class PPayTest {
 
         PosPayRecord ret = posClient.getByIdPosPay(
                 contractAddress,
-                "2nd_berth_8");
+                "2nd_berth_760");
         System.out.printf("%s\n", ret.toString());
 
+
+        Assertions.assertEquals("2nd_berth_760", ret.berthId);
+
+        PosPayRecord ret2 = posClient.getByIndexPosPay(contractAddress,4);
+        System.out.printf("%s\n", ret2.toString());
+        Assertions.assertEquals("2nd_berth_760", ret2.berthId);
+
+
         posClient.stop();
-        Assertions.assertEquals("2nd_berth_8", ret.berthId);
     }
 
 }
