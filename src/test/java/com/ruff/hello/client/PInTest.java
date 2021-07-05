f91724a8456ac51beb2908fe024a872b84a40e0f
yango3 <o3.yang@hotmail.com>
2021-07-05 15:04:31 +0800

    update PinTest

--- src/test/java/com/ruff/hello/client/PInTest.java
+++ src/test/java/com/ruff/hello/client/PInTest.java
@@ -5,7 +5,8 @@ import org.junit.jupiter.api.Assertions;
 
 public class PInTest {
     static String privateKey = "cdc7cc95755f19aa8168e2b0c3dd89d556be87b60608835549c0aee38d156640";
-    static String contractAddress = "0xc65d19deffc4c4ac83afafe479434e3306a4cfeb";
+    static String contractAddress = "0xc425800568b24eca378563db315436c4c9a6bd6b";
+    // ""0xc65d19deffc4c4ac83afafe479434e3306a4cfeb";
 
     @Test
     public void query(){
@@ -19,11 +20,17 @@ public class PInTest {
 
         PosInRecord ret = posClient.getByIdPosIn(
                 contractAddress,
-                "berth_719");
+                "berth_700");
         System.out.printf("%s\n", ret.toString());
 
+        Assertions.assertEquals("berth_700", ret.berthId);
+
+        PosInRecord ret2 = posClient.getByIndexPosIn(contractAddress, 1);
+        System.out.printf("%s\n", ret.toString());
+
+        Assertions.assertEquals("berth_700", ret.berthId);
+
         posClient.stop();
-        Assertions.assertEquals("berth_719", ret.berthId);
     }
 
 }
