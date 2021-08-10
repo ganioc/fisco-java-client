f62e786c6ad463cc16b4c83f605bfd1d7ad41132
yango3 <o3.yang@hotmail.com>
2021-08-10 15:19:56 +0800

    update

--- src/main/java/com/ruff/hello/client/PosCmd.java
+++ src/main/java/com/ruff/hello/client/PosCmd.java
@@ -2,7 +2,7 @@ package com.ruff.hello.client;
 
 public class PosCmd {
     static String privateKey = "cdc7cc95755f19aa8168e2b0c3dd89d556be87b60608835549c0aee38d156640";
-    static String contractAddress = "0x7c567f388fbe437e5707b3556ec911d0230b9d01";
+    static String contractAddress = "0xa1fe97cb847fa8cd60d6a38133c710c7ed5eefdf";
 
     private void insertPIn(){
         System.out.println("insertPIn()");
@@ -20,8 +20,6 @@ public class PosCmd {
 
         System.out.println("index is: " + rtn);
 
-        // insert a pIn record
-        InsertRecord.pIn();
 
         posClient.stop();
     }
